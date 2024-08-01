832875be74755bb6052e549afcdf8067c0c34c03
ArevaloDev <arevalodaniel999@gmail.com>
2024-07-31 22:56:54 -0600

    agregando Models, controllers, y el contexto de la base de datos en memoria

---
+++ .vs/RealStateAPI/RealStateAPI/Controllers/RealStateSalesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RealStateAPI.Models;
+
+namespace RealStateAPI.Controllers
+{
+    [Route("/api[controller]")]
+    [ApiController]
+
+    public class RealStateSalesController : ControllerBase
+    {
+        private readonly RealStateSalesContext _context;
+        public RealStateSalesController(RealStateSalesContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<HousingLocation>>> GetRealStateSales()
+        {
+            return await _context.HousingLocations.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<HousingLocation>> SendHousingLocation(HousingLocation body)
+        {
+            _context.HousingLocations.Add(body);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetRealStateSales), new
+            {
+                id = body.Id,
+                name = body.Name,
+                city = body.City,
+                state = body.State,
+                photo = body.Photo,
+                availableUnits = body.AvailableUnits,
+                wifi = body.Wifi,
+                laundry = body.Laundry
+
+            });
+        }
+    }
+}
---
+++ .vs/RealStateAPI/RealStateAPI/Models/RealStateSalesContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RealStateAPI.Models
+{
+    public class RealStateSalesContext:DbContext
+    {
+        public RealStateSalesContext(DbContextOptions<RealStateSalesContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<HousingLocation> HousingLocations { get; set; } = null!;
+    }
+}
